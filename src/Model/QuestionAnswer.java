fe832b293a2cb1f25d19eaec47371cfd157e9125
jon-jc <jonathancho.jc@gmail.com>
2021-05-17 15:22:58 -0700

    Added code to QuestionAnswer

---
+++ src/Model/QuestionAnswer.java
@@ -0,0 +1,23 @@
+public class QuestionAnswer {
+    private String question;
+    private String correctAnswer;
+    private String[] answer;
+
+    public QuestionAnswer(String question, String[] answers, String correctAnswer){
+
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getAnswer() {
+        return answer;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+
+}
